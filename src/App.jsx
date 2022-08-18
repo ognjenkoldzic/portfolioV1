7e15194cbcad27e8ca65c2b5f86e0433e5c85ee7
Ognjen Koldzic <ognjen_koldzic@yahoo.de>
2022-08-18 16:09:29 +0200

    WP basic look

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import React from "react";
+import MapFramed from "./assets/MapFrame.png";
+import GalleryFramed from "./assets/GalleryFrame.png";
+import "./App.css";
+import { Link } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className="appBody font-face-gm">
+      <h1>Portfolio</h1>
+      <img src={GalleryFramed} alt="Galleriebild mit Rahmen" />
+      <h2>...in heaven</h2>
+
+      <img src={MapFramed} alt="Kartenbild mit Rahmen" />
+      <h2>...on earth</h2>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
